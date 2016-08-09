35c9cee7b6bd20a303b74929664cb3e6a6c9b0a8
David Koller <kolldavi@gmail.com>
2016-08-09 15:01:47 +0000

    Add summit button on contacts page add to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,16 @@ class ContactsController < ApplicationController
         @contact = Contact.new
   end
   def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+        redirect_to new_contact_path, notice: "Message sent."
+      else
+        redirect_to new_contact_path, notice: "Error occured."
+      end
+  end
+  
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comments)
   end
 end
