03abab2f446646ee5b991ec9f9bd78b1b3038f64
David Koller <kolldavi@gmail.com>
2016-09-12 23:11:46 +0000

    change location

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,14 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
-    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :job_title, presence: true
+  validates :contact_email, presence: true
+
+  # paperclip profile picture
+  has_attached_file :avatar, styles: { medium: "300x300>", square: '200x200#', thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 end
