b1e2bc0d83c3d7811f21524fbdbe09bdb1d28195
David Koller <kolldavi@gmail.com>
2016-09-12 23:59:06 +0000

    added paperclip file

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,5 @@
 #no time stamp
+# config/initializers/paperclip.rb
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:use_timestamp] = false
