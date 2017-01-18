c5338b1a786cb839dc4e8da9ea4034ec0b657c86
David Koller <kolldavi@gmail.com>
2017-01-18 21:23:06 +0000

    changed about and email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'djksh3434@yahoo.com'
+   default to: 'kolldavi@gmail.com'
     def contact_email(name,email,body)
         @name = name
         @email = email
